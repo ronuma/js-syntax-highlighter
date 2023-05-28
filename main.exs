0477550bf75d974eca535fbffb068b156ee41e92
ronuma <ronum.00@gmail.com>
2023-05-27 20:31:05 -0600

    v1 of the highlighter. Pending better definition of the regular expressions

--- main.exs
+++ main.exs
@@ -3,23 +3,94 @@
 # Rodrigo Núñez Magallanes, A01028310
 # Andrea Alexandra Barrón Córdova,
 
-defmodule Highlighter do
+defmodule JSSH do
 
-# Regular expressions
-keywordEx = ~r/\b(?:abstract|await|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|with|yield)\b/
-# number (int or float)
-# boolean (true or false)
-# string (encapsulated in quotation marks)
-# comment (starts with // and finishes on newline OR starts at /* and finishes on next */)
+  def run() do
+    # ----  CONSTANTS  -----
+    in_filename = "test.js" # this will be inputted in the end
+    out_filename = "index.html"
+    doc_head = """
+    <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="stylesheet" href="styles.css" />
+          <link rel="preconnect" href="https://fonts.googleapis.com">
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+          <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+          <title>JS Syntax Highlighter</title></head><body>
+    """
+    doc_tail = "</body></html>"
 
-def highlight() do
-  # text = "await consequence = loki"
-  # if Regex.match?(keywordEx, text) do
-  #   IO.puts("Pattern matched!")
-  # else
-  #   IO.puts("Pattern not matched.")
-  # end
-  # const name=kol
-end
+    # Regular expressions
+    keywordRegex = ~r/\b(?:abstract|await|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|export|extends|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|try|typeof|var|void|volatile|while|with|yield)\b/
+    numberRegex = ~r/\b-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?\b/
+    booleanRegex = ~r/\b(?:true|false)\b/
+    stringRegex = ~r/"(?:\\.|[^"\\])*"/
+    commentRegex = ~r/(?:\/\/.*)|(?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)/
+
+    # initialize html output file
+    File.write(out_filename, doc_head)
+
+    # read js file
+    code = File.read!(in_filename)
+
+    # find and highlight keywords
+    highlighted_code =
+      code
+      # split the code by lines
+      |> String.split("\n", trim: true)
+      # map every line, if the line is empty, add a <br> tag
+      |> Enum.map(fn line ->
+        if line == "" do
+          "<br>"
+        else
+          # get the indentation of the line
+          indentation = String.split(line, ~r/\S/, trim: true) |> List.first() || ""
+          line
+          # split the line by words
+          |> String.split(~r/\b/)
+          # map every word, if the word is a token, highlight it
+          |> Enum.map(fn word ->
+            if Regex.match?(stringRegex, word) do
+              "<span class=\"string\">#{word}</span>"
+            else
+              if Regex.match?(booleanRegex, word) do
+                "<span class=\"boolean\">#{word}</span>"
+                else
+                  if Regex.match?(numberRegex, word) do
+                    "<span class=\"number\">#{word}</span>"
+                  else
+                    if Regex.match?(keywordRegex, word) do
+                      "<span class=\"keyword\">#{word}</span>"
+                    else
+                      if Regex.match?(commentRegex, word) do
+                        "<span class=\"comment\">#{word}</span>"
+                      else
+                        "<span class=\"standard\">#{word}</span>"
+                      end
+                    end
+                  end
+                end
+              end
+          end)
+          # join the words again
+          |> Enum.join("")
+          # add the indentation to the line
+          |> String.replace_prefix("", indentation)
+        end
+      end)
+      # join the lines again with a <br> tag
+      |> Enum.join("<br>")
+
+    # write the highlighted code to the html file
+    File.write(out_filename, highlighted_code, [:append])
+
+    # close the tags of the html file and finish
+    File.write(out_filename, doc_tail, [:append])
+
+  end
 
 end
--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-const yo = "abundalacaaca"
+/* 
+Test file v1 for the JS comment
+*/
+
+const test = "Hello world"
+const amTesting = true
+const testNumber = 123.52
+// test comment
+if (amTesting) {
+   console.log(test)
+   console.log(testNumber) // test comment
+}
