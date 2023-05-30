9704b9521faf55a06914409421f98f9bc4f88ddd
ronuma <ronum.00@gmail.com>
2023-05-30 08:53:49 -0600

    getting very close

--- main.exs
+++ main.exs
@@ -22,20 +22,30 @@ defmodule JSSH do
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
           <title>JS Syntax Highlighter</title></head><body><pre>
     """
-    doc_tail = "</pre></body></html>"
-
-    # Regular expressions
-    commentRegex = ~r/\/\/(|\r)*|\/\*(.|\n|\r)*\*\//
-    stringRegex = ~r/(["'])(?:(?=(\\?))\2.)*?\1/
-    keywordRegex = ~r/\b(?:abstract|await|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|export|extends|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|try|typeof|var|void|volatile|while|with|yield)\b/
-    numberRegex = ~r/\b-?\d+\.?(\d+)?\b/
-    booleanRegex = ~r/\b(?:true|false)\b/
+    # doc_tail = "</pre></body></html>"
 
     # initialize html output file
     File.write(out_filename, doc_head)
 
     # read js file
     code = File.read!(in_filename)
+
+    write_file(code, out_filename)
+    # # close the tags of the html file and finish
+    # File.write(out_filename, doc_tail, [:append])
+
+  end
+
+  defp write_file("", out_filename), do: File.write(out_filename, "</pre></body></html>", [:append])
+
+  defp write_file(code, out_filename) do
+    IO.puts(code)
+    # Regular expressions
+    commentRegex = ~r/\/\/(|\r)*|\/\*(.|\n|\r)*\*\//
+    stringRegex = ~r/(["'])(?:(?=(\\?))\2.)*?\1/
+    keywordRegex = ~r/\b(?:abstract|await|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|export|extends|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|try|typeof|var|void|volatile|while|with|yield)\b/
+    numberRegex = ~r/\b-?\d+\.?(\d+)?\b/
+    booleanRegex = ~r/\b(?:true|false)\b/
 
     code
       # split the code by lines
@@ -53,59 +63,60 @@ defmodule JSSH do
             if Regex.match?(~r/\/\*(.|\n|\r)*/, word) do
               result = Regex.run(commentRegex, code)
               html = "<span class=\"comment\">#{result}</span>"
-              code = Regex.replace(commentRegex, code, "")
               File.write(out_filename, html, [:append])
+              code = Regex.replace(commentRegex, code, "")
+              write_file(code, out_filename)
             else
             if Regex.match?(~r/\/\/.*/, word) do
               result = Regex.run(commentRegex, code)
               html = "<span class=\"comment\">#{result}</span>"
-              code = Regex.replace(commentRegex, code, "")
               File.write(out_filename, html, [:append])
+              code = Regex.replace(commentRegex, code, "")
+              write_file(code, out_filename)
             else
-              if Regex.match?(~r/.*["']/, word) do
+              if Regex.match?(stringRegex, word) do
                 result = Regex.run(stringRegex, code)
                 html = "<span class=\"string\">#{result}</span>"
-                code = Regex.replace(stringRegex, code, "")
                 File.write(out_filename, html, [:append])
+                code = Regex.replace(stringRegex, code, "")
+                write_file(code, out_filename)
               else
                   if Regex.match?(keywordRegex, word) do
                     result = Regex.run(keywordRegex, code)
                     html = "<span class=\"keyword\">#{result}</span>"
-                    code = Regex.replace(keywordRegex, code, "")
                     File.write(out_filename, html, [:append])
+                    code = Regex.replace(keywordRegex, code, "")
+                    write_file(code, out_filename)
                   else
                     if Regex.match?(numberRegex, word) do
                       result = Regex.run(numberRegex, code)
                       html = "<span class=\"number\">#{result}</span>"
-                      code = Regex.replace(numberRegex, code, "")
                       File.write(out_filename, html, [:append])
+                      code = Regex.replace(numberRegex, code, "")
+                      write_file(code, out_filename)
                     else
                       if Regex.match?(booleanRegex, word) do
                         result = Regex.run(booleanRegex, code)
                         html = "<span class=\"boolean\">#{result}</span>"
-                        code = Regex.replace(booleanRegex, code, "")
                         File.write(out_filename, html, [:append])
+                        code = Regex.replace(booleanRegex, code, "")
+                        write_file(code, out_filename)
                       else
                         result = word
                         html = "<span>#{result}</span>"
                         File.write(out_filename, html, [:append])
+                        # TODO : remove the word from the code
+                        code = Regex.replace(word, code, "")
+                        write_file(code, out_filename)
                       end
                     end
-                  end
+                    end
                   end
                 end
               end
           end)
-          # join the words again
-          |> Enum.join("")
         end
       end)
-      # join the lines again with a <br> tag
-      |> Enum.join("<br>")
-
-    # close the tags of the html file and finish
-    File.write(out_filename, doc_tail, [:append])
-
   end
 
 end
