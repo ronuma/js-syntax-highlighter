f020794b4191254f5fd781211f188b59e5a0bda6
ronuma <ronum.00@gmail.com>
2023-05-30 20:11:12 -0600

    almost working version with no multi line comments

--- main.exs
+++ main.exs
@@ -1,11 +1,10 @@
 # JavaScript syntax highlighter
 
 # Rodrigo Núñez Magallanes, A01028310
-# Andrea Alexandra Barrón Córdova,
+# Andrea Alexandra Barrón Córdova, A01783126
 
 defmodule JSSH do
   def run() do
-    # ----  CONSTANTS  -----
     in_filename = "test.js" # this will be inputted in the end
     out_filename = "index.html"
     doc_head = """
@@ -32,124 +31,94 @@ defmodule JSSH do
 
   end
 
-  defp write_file("", out_filename), do: File.write(out_filename, "</pre></body></html>", [:append])
-
   defp write_file(code, out_filename) do
-    IO.puts("CODE: #{code}")
-    # Regular expressions
-    commentRegex = ~r/\/\/(|\r)*|\/\*(.|\n|\r)*\*\//
-    stringRegex = ~r/(["'])(?:(?=(\\?))\2.)*?\1/
-    keywordRegex = ~r/\b(?:abstract|await|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|export|extends|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|try|typeof|var|void|volatile|while|with|yield)\b/
-    numberRegex = ~r/\b-?\d+\.?(\d+)?\b/
-    booleanRegex = ~r/\b(?:true|false)\b/
-    equalRegex = ~r/=/
-
     code
     # split the code by lines
     |> String.split("\n")
+    # remove trailing spaces and \r
     |> Enum.map(&String.trim_trailing/1)
-    # map every line, if the line is empty, add a <br> tag
     |> Enum.map(fn line ->
-        line
-        # split the line by words
-        |> String.split(~r/\b/) # TODO: change this separation
-        # map every word, if the word is a token, highlight it
-        |> Enum.map(fn word ->
-          IO.inspect(word)
-          if Regex.match?(~r/\/\*(.|\n|\r)*/, word) do
-            IO.puts("COMENTARIO MULTILINEA DETECTADO")
-            result = Regex.run(commentRegex, code)
-            html = "<span class=\"comment\">#{result}</span>"
-            File.write(out_filename, html, [:append])
-            code = Regex.replace(commentRegex, code, "", global: false)
-            # throw :stop
-            write_file(code, out_filename)
-          else
-            if Regex.match?(~r/\/\/.*/, word) do
-              IO.puts("COMENTARIO DETECTADO")
-              result = Regex.run(commentRegex, code)
-              html = "<span class=\"comment\">#{result}</span>"
-              File.write(out_filename, html, [:append])
-              code = Regex.replace(commentRegex, code, "", global: false)
-              # throw :stop
-              write_file(code, out_filename)
-            else
-              if Regex.match?(~r/.*=/, word) do
-                        IO.puts("EQUAL SIGN DETECTED")
-                        result = Regex.run(equalRegex, code)
-                        html = "<span class=\"boolean\">#{result}</span>"
-                        File.write(out_filename, html, [:append])
-                        code = Regex.replace(equalRegex, code, "", global: false)
-                        # throw :stop
-                        write_file(code, out_filename)
-              else
-                 if Regex.match?(~r/["']/, word) do
-                        IO.puts("STRING DETECTED")
-                        result = Regex.run(stringRegex, code)
-                        html = "<span class=\"string\">#{result}</span>"
-                        File.write(out_filename, html, [:append])
-                        code = Regex.replace(stringRegex, code, "", global: false)
-                        # throw :stop
-                        write_file(code, out_filename)
-                 else
-                if Regex.match?(keywordRegex, word) do
-                  IO.puts("PALABRA CLAVE DETECTADO")
-                  result = Regex.run(keywordRegex, code)
-                  html = "<span class=\"keyword\">#{result}</span>"
-                  File.write(out_filename, html, [:append])
-                  code = Regex.replace(keywordRegex, code, "", global: false)
-                  # throw :stop
-                  write_file(code, out_filename)
-                else
-                  if Regex.match?(numberRegex, word) do
-                    IO.puts("NUMERO DETECTADO")
-                    result = Regex.run(numberRegex, code)
-                    html = "<span class=\"number\">#{result}</span>"
-                    File.write(out_filename, html, [:append])
-                    code = Regex.replace(numberRegex, code, "", global: false)
-                    # throw :stop
-                    write_file(code, out_filename)
-                  else
-                    if Regex.match?(booleanRegex, word) do
-                      IO.puts("BOOLEANO DETECTADO")
-                      result = Regex.run(booleanRegex, code)
-                      html = "<span class=\"boolean\">#{result}</span>"
-                      File.write(out_filename, html, [:append])
-                      code = Regex.replace(booleanRegex, code, "", global: false)
-                      throw :stop
-                      write_file(code, out_filename)
-                      else
-                        IO.puts("NO CUSTOM REGEX MATCH DETECTED")
-                      # didn't match any of the tokens, just write the word
-                      if Regex.match?(~r/\r/, word) do
-                        IO.puts("ENTER DETECTED")
-                        # aquí sí reemplaza el \r por nada
-                        code = String.replace(code, "\r", "", global: false)
-                        # throw :stop
-                        write_file(code, out_filename)
-                      else
-                        if word == "" do
-                          IO.puts("EMPTY STRING DETECTED")
-                          :skip
-                        else
-                          IO.puts("WORD OR SPACE DETECTED")
-                          html = "<span>#{word}</span>"
-                          File.write(out_filename, html, [:append])
-                          # Remove the word from the code
-                          code = String.replace(code, word, "", global: false)
-                          # throw :stop
-                          write_file(code, out_filename)
-                        end
-                      end
-                    end
-                    end
-                  end
-                end
-              end
-            end
-          end
-      end)
+      inspect_line(line, out_filename)
     end)
+  end
+
+  defp inspect_line("", out_filename), do: File.write(out_filename, "<br>", [:append])
+  defp inspect_line(line, out_filename) do
+    # Regular expressions
+    commentRegex = ~r/^\/\/.*|\/\*(.|\n|\r)*\*\//
+    stringRegex = ~r/^(["'])(?:(?=(\\?))\2.)*?\1/
+    keywordRegex = ~r/^\b(?:abstract|await|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|export|extends|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|try|typeof|var|void|volatile|while|with|yield)\b/
+    numberRegex = ~r/^\b-?\d+\.?(\d+)?\b/
+    booleanRegex = ~r/^\b(?:true|false)\b/
+    equalRegex = ~r/^=/
+    spaceRegex = ~r/^\s+/
+    varRegex = ~r/^[a-zA-Z_$][a-zA-Z0-9_$]*/
+    parenRegex = ~r/^\(|\)/
+
+    cond do
+      Regex.match?(commentRegex, line) ->
+        IO.puts("COMMENT DETECTED")
+        [head | _] = Regex.run(commentRegex, line)
+        html = "<span class=\"comment\">#{head}</span>"
+        File.write(out_filename, html, [:append])
+        line = Regex.replace(commentRegex, line, "", global: false)
+        inspect_line(line, out_filename)
+      Regex.match?(stringRegex, line) ->
+        IO.puts("STRING DETECTED")
+        [head | _] = Regex.run(stringRegex, line)
+        html = "<span class=\"string\">#{head}</span>"
+        File.write(out_filename, html, [:append])
+        line = Regex.replace(stringRegex, line, "", global: false)
+        inspect_line(line, out_filename)
+      Regex.match?(keywordRegex, line) ->
+        IO.puts("KEYWORD DETECTED")
+        [head | _] = Regex.run(keywordRegex, line)
+        html = "<span class=\"keyword\">#{head}</span>"
+        File.write(out_filename, html, [:append])
+        line = Regex.replace(keywordRegex, line, "", global: false)
+        inspect_line(line, out_filename)
+      Regex.match?(spaceRegex, line) ->
+        IO.puts("SPACE DETECTED")
+        [head | _] = Regex.run(spaceRegex, line)
+        html = "<span class=\"space\">#{head}</span>"
+        File.write(out_filename, html, [:append])
+        line = Regex.replace(spaceRegex, line, "", global: false)
+        inspect_line(line, out_filename)
+      Regex.match?(varRegex, line) ->
+        IO.puts("VAR DETECTED")
+        [head | _] = Regex.run(varRegex, line)
+        html = "<span>#{head}</span>"
+        File.write(out_filename, html, [:append])
+        line = Regex.replace(varRegex, line, "", global: false)
+        inspect_line(line, out_filename)
+      Regex.match?(equalRegex, line) ->
+        IO.puts("EQUAL DETECTED")
+        [head | _] = Regex.run(equalRegex, line)
+        html = "<span class=\"boolean\">#{head}</span>"
+        File.write(out_filename, html, [:append])
+        line = Regex.replace(equalRegex, line, "", global: false)
+        inspect_line(line, out_filename)
+      Regex.match?(booleanRegex, line) ->
+        IO.puts("BOOLEAN DETECTED")
+        [head | _] = Regex.run(booleanRegex, line)
+        html = "<span class=\"boolean\">#{head}</span>"
+        File.write(out_filename, html, [:append])
+        line = Regex.replace(booleanRegex, line, "", global: false)
+        inspect_line(line, out_filename)
+      Regex.match?(numberRegex, line) ->
+        IO.puts("NUMBER DETECTED")
+        [head | _] = Regex.run(numberRegex, line)
+        html = "<span class=\"number\">#{head}</span>"
+        File.write(out_filename, html, [:append])
+        line = Regex.replace(numberRegex, line, "", global: false)
+        inspect_line(line, out_filename)
+      true ->
+        IO.puts("NO DEFINED MATCH DETECTED")
+        html = "<span>#{line}</span>"
+        File.write(out_filename, html, [:append])
+        line = String.replace(line, line, "", global: false)
+        inspect_line(line, out_filename)
+    end
   end
 
 end
--- test.js
+++ test.js
@@ -1,11 +1,8 @@
-/* 
-Test file v1 for the JS comment
-"should be ignored" it is false that it shouldn't 56
-*/
 const test = "Hello world for test.js"
 const amTesting = true
 const testNumber = 123.52 // another test comment "right"
 let anotherNumber = 0
+
 // test comment
 if (amTesting) {
    console.log(test)
